c35bb5d0c82f3d3c2c1d64dd15037d1a9da859d4
hyannu <annurao9560@gmail.com>
2026-06-30 01:28:26 +0530

    used query selector all

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // let heading = document.getElementById("heading");
 // console.log(heading);
 
-let heading = document.getElementsByClassName("heading");
+/*let heading = document.getElementsByClassName("heading");
 console.dir(headings);
-console.log(headings);
+console.log(headings);*/
+
+let elements = document.querySelector("p");
+console.dir(elements);
