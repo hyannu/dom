faf6e2b7cb5e0c212a9b904b743491e583ea9c81
hyannu <annurao9560@gmail.com>
2026-06-30 01:25:42 +0530

    DOM manipulation

---
+++ script.js
@@ -0,0 +1,6 @@
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+let heading = document.getElementsByClassName("heading");
+console.dir(headings);
+console.log(headings);
