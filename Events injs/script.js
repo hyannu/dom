b00029ace591d51e7374a8b324560ceac6e4b8fd
hyannu <annurao9560@gmail.com>
2026-06-30 19:04:20 +0530

    Add mouseover event to div

--- Events injs/script.js
+++ Events injs/script.js
@@ -1,5 +1,13 @@
 let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
 console.log("btn1 was clicked");
 let a = 25;
 a++;
 console.log(a);//26
+};
+
+let box = document.querySelector("div");
+div.onmouseover = () => {
+    console.log("you are inside div");
+};
