42be920902cc217a590f77cb3f9e971957d47339
hyannu <annurao9560@gmail.com>
2026-06-30 23:58:02 +0530

    Implement light and dark mode switch

--- Events injs/script.js
+++ Events injs/script.js
@@ -48,3 +48,23 @@
 // // };
 
 
+
+
+let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light";//dark
+
+modeBtn.addEventListener("click", () => {
+    if (currMode === "light") {
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+        // document.querySelector("body").classList.add("dark");
+    }else {
+        currMode ="light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+                document.querySelector("body").style.backgroundColor = "white";
+    }
+    console.log(currMode);
+});
