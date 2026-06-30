c3a55364e657981efb7538d2c7587ac212a45611
hyannu <annurao9560@gmail.com>
2026-06-30 23:30:57 +0530

    question

--- Events injs/script.js
+++ Events injs/script.js
@@ -1,48 +1,50 @@
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-// btn1.onclick = (e) => {
+// // btn1.onclick = (e) => {
+// //     console.log(e);
+// //     console.log(e.type);
+// //     console.log(e.target);
+// //     console.log(e.clientX,e.clientY);
+// // };
+
+// btn1.addEventListener("click", (e) =>{
+//     console.log("button1 was clicked-header1");
 //     console.log(e);
 //     console.log(e.type);
-//     console.log(e.target);
-//     console.log(e.clientX,e.clientY);
+
+// });
+
+
+// btn1.addEventListener("click", () =>{
+//     console.log("button1 was clicked-headler2");
+// });
+
+// const handler3 = () => {
+//     console.log("button1 was clicked-headler3");
 // };
 
-btn1.addEventListener("click", (e) =>{
-    console.log("button1 was clicked-header1");
-    console.log(e);
-    console.log(e.type);
+// btn1.addEventListener("click", headler3);
 
-});
-
-
-btn1.addEventListener("click", () =>{
-    console.log("button1 was clicked-headler2");
-});
-
-const handler3 = () => {
-    console.log("button1 was clicked-headler3");
-};
-
-btn1.addEventListener("click", headler3);
-
-btn1.addEventListener("click", () =>{
-    console.log("button1 was clicked-headler4");
-});
+// btn1.addEventListener("click", () =>{
+//     console.log("button1 was clicked-headler4");
+// });
 
 
-btn1.addEventListener("click", headler3);
-//  let div = document.querySelector("div");
+// btn1.addEventListener("click", headler3);
+// //  let div = document.querySelector("div");
 
-/*console.log("btn1 was clicked");
-let a = 25;
-a++;
-console.log(a);//26
-};*/
+// /*console.log("btn1 was clicked");
+// let a = 25;
+// a++;
+// console.log(a);//26
+// };*/
 
-// let div = document.querySelector("div");
-// div.onmouseover = (e) => {
-//     console.log(e);
-//     console.log(e.type);
-//     console.log(e.target);
-//     console.log(e.clientX,e.clientY);
-// };
+// // let div = document.querySelector("div");
+// // div.onmouseover = (e) => {
+// //     console.log(e);
+// //     console.log(e.type);
+// //     console.log(e.target);
+// //     console.log(e.clientX,e.clientY);
+// // };
+
+
